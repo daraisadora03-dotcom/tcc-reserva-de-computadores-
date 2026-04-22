710ecccadb28b99b9aff0b284bf7725cc5f28371
richardguir9000 <richardguir9000@gmail.com>
2026-04-22 18:22:26 -0300

    teste

--- tcc-reserva-de-computadores-/enviar_para_github.bat
+++ tcc-reserva-de-computadores-/enviar_para_github.bat
@@ -11,8 +11,8 @@ set /p msg="Digite a mensagem do seu commit: "
 if not exist .git (
     echo [INFO] Inicializando o Git...
     git init
-    set /p url="Cole a URL do seu repositorio GitHub: "
-    git remote add origin %url%
+    set /p url="Cole a URL do seu repositorio GitHub (https://github.com/...): "
+    if not "%url%"=="" git remote add origin "%url%"
 )
 
 :: Verifica se o remote 'origin' existe (caso a pasta .git ja existisse mas sem o link)
@@ -20,23 +20,22 @@ git remote get-url origin >nul 2>&1
 if %errorlevel% neq 0 (
     echo [AVISO] Link do GitHub nao encontrado.
     set /p url="Cole a URL do seu repositorio GitHub (https://github.com/...): "
-    git remote add origin %url%
+    if not "%url%"=="" git remote add origin "%url%"
 )
 
 echo [INFO] Sincronizando com o GitHub (Pull)...
-git pull origin main --rebase
+:: Tenta sincronizar, mas ignora erros caso o repositorio remoto esteja vazio
+git pull origin main --rebase >nul 2>&1
 
 echo [INFO] Corrigindo pastas aninhadas e limpando cache...
-:: Remove pastas .git internas (submodulos fantasmas) que travam o 'git add'
-for /d %%D in (*) do (
-    if /i "%%D" neq ".git" (
-        for /f "delims=" %%F in ('dir /s /b /ad "%%D\.git" 2^>nul') do (
-            attrib -h -r -s "%%F" /s /d
-            rd /s /q "%%F"
-        )
+:: Remove pastas .git internas de forma silenciosa para evitar erros de console
+for /f "tokens=*" %%i in ('dir /s /b /ad .git 2^>nul') do (
+    if /i "%%i" neq "%CD%\.git" (
+        attrib -h -r -s "%%i" /s /d >nul 2>&1
+        rd /s /q "%%i" >nul 2>&1
     )
 )
-:: Limpa o cache do Git para garantir que as pastas sejam tratadas como arquivos comuns
+:: Limpa o cache do Git silenciosamente para destravar o upload
 git rm -r --cached . >nul 2>&1
 
 echo [INFO] Adicionando arquivos...
